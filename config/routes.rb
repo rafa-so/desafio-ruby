81f662b939d3084aaf7af4c40fbc108a7a518c47
Rafael Oliveira <rafa.forsoft.25@gmail.com>
2017-12-23 09:50:52 -0200

    apresentação da tela de login, e cadastro de usuário

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   #get 'home/index'
   root 'home#index'
 
-  devise_for :users
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
